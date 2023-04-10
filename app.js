273b3d0ee55033d8e58cbf00a8269f7df36e25a7
Nate Stemo <natestemo@hotmail.com>
2023-04-10 13:59:30 -0500

    waits for client's requested resource to load

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ async function start(){
     let url = 'https://usaweightlifting.sport80.com/public/rankings/results/5738'
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(url)
+    await page.goto(url, {
+        waitUntil: 'networkidle0'
+    })
 
     await page.screenshot({path: 'meet.png', fullPage: true})
 }
