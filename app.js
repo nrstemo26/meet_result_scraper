aa71ba952fd601144da9b33e849bed390b983317
Nate Stemo <natestemo@hotmail.com>
2023-04-10 14:10:31 -0500

    retrieves header elements

--- app.js
+++ app.js
@@ -8,7 +8,17 @@ async function start(){
         waitUntil: 'networkidle0'
     })
 
-    await page.screenshot({path: 'meet.png', fullPage: true})
+    // await page.screenshot({path: 'meet.png', fullPage: true})
+
+    const tableHeaderData = await page.evaluate(()=>{
+        let elArr = Array.from(document.querySelectorAll(".data-table div div.v-data-table div.v-data-table__wrapper table thead tr th > span"))
+        elArr = elArr.map((x)=>{
+            return  x.textContent
+        })
+        return elArr
+    })
+    console.log(tableHeaderData)
+
 }
 
 start()
