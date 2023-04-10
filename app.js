35166cb712323328640809ae38f040079cd34a80
Nate Stemo <natestemo@hotmail.com>
2023-04-10 13:58:05 -0500

    launches new browser page and gets screenshot

---
+++ app.js
@@ -0,0 +1,12 @@
+const puppeteer = require('puppeteer')
+
+async function start(){
+    let url = 'https://usaweightlifting.sport80.com/public/rankings/results/5738'
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url)
+
+    await page.screenshot({path: 'meet.png', fullPage: true})
+}
+
+start()
