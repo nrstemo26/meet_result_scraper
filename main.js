0091206f8b64d1268dee7db56a7df3e56f204ad7
Nate Stemo <natestemo@hotmail.com>
2023-08-04 10:53:08 -0500

    pseudocode for main process for scraping new meets

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+//what are the main processes we need to do for this
+
+//have function that scrapes 1 meet off of urlID
+
+//get all meet meta-data
+//compare to previous week's metadata
+//filter out all overlapping metadata
+
+// take that list/array/csv and loop thru the 
+// https://usaweightlifting.sport80.com/public/rankings/results/ 
+// click to open(preferably in a new tab)
+// get url#? of meet and add to metadata
+
+
