5e13d7f794d9f427ef467dfafa2d24ab55e2b4c6
Nate Stemo <natestemo@hotmail.com>
2023-08-07 11:08:37 -0500

    refactor

--- meetMetadataCsv.js
+++ meetMetadataCsv.js
@@ -53,7 +53,7 @@ async function readCSV(file) {
       .on('error', (error) => {
         reject(error);
     });
-    });
+ });
 }
 
 async function saveCSV(file, data, headers) {
@@ -134,19 +134,6 @@ async function makeNewMeetMetaData(){
 module.exports = {
     makeNewMeetMetaData: makeNewMeetMetaData,
 }
-
-//what are the main processes we need to do for this
-
-//have function that scrapes 1 meet off of urlID
-
-//get all meet meta-data
-//compare to previous week's metadata
-//filter out all overlapping metadata
-
-// take that list/array/csv and loop thru the 
-// https://usaweightlifting.sport80.com/public/rankings/results/ 
-// click to open(preferably in a new tab)
-// get url#? of meet and add to metadata
 
 
 
