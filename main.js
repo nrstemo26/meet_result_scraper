e6e47d7a5c782f9b74a1f0d4bc9f7f01e2974bbd
Nate Stemo <natestemo@hotmail.com>
2023-08-09 13:24:28 -0500

    scrapes meets off of new meet csv

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const csv = require('csv-parser');
-const { makeNewMeetMetaData } = require('./meetMetadataCsv')
-
+const { makeNewMeetMetaData,addUrlToMeetCsv} = require('./meetMetadataCsv')
+const {searchForNewMeets} = require('./searchForNewMeets')
+const {start:getOneMeetCSV} = require('./app')
 // Specify the input CSV file path
 
 async function getNewMeetArray (inputCsvPath, callback){
@@ -28,10 +29,29 @@ async function getNewMeetArray (inputCsvPath, callback){
 //now this needs to read that csv get the meet name and have that as an array
 
 async function run (){
-    console.log('getting new meet metadata')
+    // console.log('getting new meet metadata')
     const fileName = await makeNewMeetMetaData()
-    const newMeetNames = await getNewMeetArray(fileName)
-    console.log('we have new meets:', newMeetNames)
+    const newMeetNamesUrl = await searchForNewMeets(await getNewMeetArray(fileName))
+    console.log(newMeetNamesUrl.length)
+
+    //write url to new metadata csv   
+    //needs a better url route
+    await addUrlToMeetCsv(fileName,'foo.csv', newMeetNamesUrl)
+    .then(() => {
+        console.log('Modified CSV saved as output.csv');
+    })
+    .catch((err) => {
+        console.error('An error occurred:', err);
+    });
+    
+    //now wee need to use the csv or the array i have 
+    //to scrape the the meet data off of the meet url
+    for(let i=0; i < newMeetNamesUrl.length; i++){
+        let meetUrl = newMeetNamesUrl[i][1]
+        await getOneMeetCSV(meetUrl, `meet-${meetUrl}`)
+        console.log('meeturl', meetUrl )
+    }
+    
     console.log('done')
 }
 
@@ -41,9 +61,9 @@ run()
 
 //have function that scrapes 1 meet off of urlID
 
-//get all meet meta-data
-//compare to previous week's metadata
-//filter out all overlapping metadata
+//DONE//get all meet meta-data
+//DONE//compare to previous week's metadata
+//DONE//filter out all overlapping metadata
 
 // take that list/array/csv and loop thru the 
 // https://usaweightlifting.sport80.com/public/rankings/results/ 
