435ff3863c16c9cd5f2ca9f48dc499ca92b363e9
Nate Stemo <natestemo@hotmail.com>
2023-08-10 20:23:18 -0500

    adds meet link url to csv

--- meet_metadata_scraper/getAllMeetMetaData.js
+++ meet_metadata_scraper/getAllMeetMetaData.js
@@ -1,14 +1,15 @@
 const puppeteer = require('puppeteer')
 const { createCSVfromArray, writeCSV } = require('../utils/csv_utils');
-const {getDateMMDDYYYY} = require('../utils/date_utils');
 const { write } = require('fs');
 
+
+//this could just grab the meet url?
 async function getAllMeetMetaData(csvName){
-    //we dont need a meet number
-    //the baseurl should preload some shit
+    console.log('running metadata scraper')
+    
     
     let url = 'https://usaweightlifting.sport80.com/public/rankings/results/'
-    // let url = baseUrl + meetNumber;
+    
    
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
@@ -16,7 +17,6 @@ async function getAllMeetMetaData(csvName){
     await page.goto(url, {
         waitUntil: 'networkidle0'
     })
-    //good to here
 
     async function getPageData(){
         return await page.$eval(
@@ -24,81 +24,67 @@ async function getAllMeetMetaData(csvName){
             x =>  x.textContent
         )
     }
-
-    //adjust the date situation
-    //get filter button  
-    // click filter button
-    //take screenshot
-    //get date start range button
-    //click date start range button
-    //move backwards in time
-    
+   
     async function moveBackMonth(){
-        // get the date range inner text
-        console.log('working our way back')
-        await page.waitForTimeout(2000)
-        page.click("div.v-date-picker-header button.v-btn.v-btn--icon.v-btn--round.theme--light.v-size--default")
-        // selector 'div.v-dialog div.v-card div.v-card__text div.s80-date-picker div.v-input div.v-input__control div.v-input__Slot div.v-text-field label  '
-        // text should say Date Range Start
+        await page.click("div.v-date-picker-header button.v-btn.v-btn--icon.v-btn--round.theme--light.v-size--default")
     }
+
     async function clickDate(){
         console.log('getting the date')
-        await page.waitForTimeout(2000)
-        const bar = await page.$eval("div.v-date-picker-table table tbody tr td:nth-of-type(1) button.v-btn div.v-btn__content", el => el.textContent)
-        // console.log(bar)
-        page.click("div.v-date-picker-table table tbody tr td:nth-of-type(1) button.v-btn div.v-btn__content")  
+        await page.waitForSelector('div.v-date-picker-table table tbody tr td:nth-of-type(1) button.v-btn div.v-btn__content')
+        await page.click("div.v-date-picker-table table tbody tr td:nth-of-type(1) button.v-btn div.v-btn__content")  
     }
 
+    //apply should always be there. after clicking the date so we might not need to 
     async function clickApply(){
         console.log('clicking apply')
-        await page.waitForTimeout(2000)
-        page.click("div.v-card__actions.justify-end button.primary.my-2.v-btn.v-btn--is-elevated")  
-    
+        await page.waitForSelector("div.v-card__actions.justify-end button.primary.my-2.v-btn.v-btn--is-elevated")
+        await page.click("div.v-card__actions.justify-end button.primary.my-2.v-btn.v-btn--is-elevated", {
+            waitUntil: 'networkidle0'
+        })     
     }
 
     async function clickFilter(){
         console.log('clicking filter button')
-        await Promise.all([
-            page.waitForNetworkIdle(),
-            page.click('.data-table div.container.pb-0 div.s80-filter div.row.no-gutters .v-badge button.v-btn'),
-        ]);
-
-        await page.waitForTimeout(2000)
-        
-        const bar = await page.$eval("#date_range_start", el => el.value)
-        // console.log(bar)
-        
-        page.screenshot({path: 'filter.png', fullPage: true})
-        
-        //this clicks and opens the date selector
-        await page.click('#date_range_start')
-
-        for(let i=0; i<3; i++){
-            console.log(i)
-            await moveBackMonth()
-        }
-        
-        await clickDate()
-        await clickApply()
-        await page.waitForTimeout(5000)         
+        await page.click('.data-table div.container.pb-0 div.s80-filter div.row.no-gutters .v-badge button.v-btn');      
     }
-
-
     
     await clickFilter()
-    // console.log('taking screenshot')
-    // await page.screenshot({path: 'meet.png', fullPage: true})
 
+    //waits for the date picker to be available and then clicks it
+    await page.waitForSelector('#date_range_start', {visible:true})
+    await page.click('#date_range_start')
+    
+    //waits for the < button to be visible and clickable
+    await page.waitForSelector('div.v-date-picker-header__value div.accent--text', { visible:true })
+    
+    let month = '';
+    //it finds the month name being January 2011
+    while(month != 'January 2021'){
+        // console.log('in loop')
+        await moveBackMonth();
+        month = await page.evaluate(()=>{
+            return document.querySelector('div.v-date-picker-header__value div.accent--text button').textContent.trim()
+        })
+        console.log(month)
+    }
+    console.log('got to', month)
+    
 
+    await clickDate()
+    await clickApply() 
+    
+    //waits for the data to actually load before we get all of the meet data
+    await page.waitForNetworkIdle()
 
     const tableHeaderData = await page.evaluate(()=>{
         let elArr = Array.from(document.querySelectorAll(".data-table div div.v-data-table div.v-data-table__wrapper table thead tr th > span"))
         elArr = elArr.map((x)=>{
             return  x.textContent
         })
-        return elArr
+        return [...elArr.slice(0,-1),'Meet Url']
     })
-    let headerCSV = tableHeaderData.join(', ');
+    let headerCSV = tableHeaderData.join('| ');
     headerCSV += '\n'
     writeCSV('meet-metadata',csvName, headerCSV);
 
@@ -122,19 +108,47 @@ async function getAllMeetMetaData(csvName){
 
     await browser.close();
 }
+//not working with the index situation
+async function getMeetUrl(index, page){
+
+    //click on a random element first
+    await page.click('h2.flex-shrink-0.align-self-end.subtitle-1', {waitUntil:'visible'})
+  
+    
+    //click the elipses button on the specific element
+    await page.click(`tbody tr:nth-of-type(${index}) td.text-end button.v-btn.v-btn--icon`)        
+    
+    //wait for the view button to pop up
+    let viewBtnSelector = 'div.v-menu__content.menuable__content__active div.v-list.v-sheet div a div.v-list-item__content div.v-list-item__title';
+    await page.waitForSelector(viewBtnSelector, {waitUntil:'visible'})
+    
+    
+    //get the href value of the view button
+    let viewBtn = 'div.v-menu__content.menuable__content__active div.v-list.v-sheet div a';
+    const meetHref = await page.$eval(viewBtn, anchor => anchor.getAttribute('href'));
+    const meetHrefNum = meetHref.split('/')[4]
+    
+    console.log(meetHrefNum)
+    return meetHrefNum;
+}
 
 async function getMeetsOnPage(athletesOnPage, page , csvName){
     let allAthleteData =[];
     for(let i = 1; i <= athletesOnPage; i++){
+        console.log(i)
+        let meetUrl = await getMeetUrl(i, page);
         let athleteData = await page.evaluate((index)=>{
             let selector = ".data-table div div.v-data-table div.v-data-table__wrapper table tbody tr:nth-of-type("+ index +") td > div"
             let elArr = Array.from(document.querySelectorAll(`${selector}`))
             elArr = elArr.map((x)=>{
-                return  x.textContent
+                return  x.textContent.trim()
             })
+
             return elArr
         },i)
-        athleteData = athleteData.map(x=> x.replace(',',' ').trim())
+        
+        athleteData[athleteData.length-1] = meetUrl
+        console.log(athleteData)
         allAthleteData.push(athleteData)
     }
 
@@ -152,9 +166,12 @@ function handleTotalAthleteString(str){
 }
 
 
+module.exports = {
+    getAllMeetMetaData: getAllMeetMetaData
+}
 
-console.log(getDateMMDDYYYY())
    
 //all-meets-MM-DD-YYYYY
 
-getAllMeetMetaData('foo-'+ getDateMMDDYYYY())
+//getAllMeetMetaData('foo-'+ getDateMMDDYYYY())
+// getAllMeetMetaData('foo-7-28-2023')
