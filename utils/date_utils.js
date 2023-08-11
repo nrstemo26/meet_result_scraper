5caea6b87623ae2e588103d317641c88dca21b2c
Nate Stemo <natestemo@hotmail.com>
2023-08-11 06:12:13 -0500

    util to get current year and what week of the year it is

--- utils/date_utils.js
+++ utils/date_utils.js
@@ -2,6 +2,19 @@ function getDateMMDDYYYY(){
     let d = new Date()
     return `${d.getMonth()+1}-${d.getDate()}-${d.getFullYear()}`
 }
+
+function getWeekandYear(){
+    const today = new Date();
+    const currentWeek = Math.ceil(
+      (today - new Date(today.getFullYear(), 0, 1)) / (7 * 24 * 60 * 60 * 1000)
+    );
+    return {
+        year: today.getFullYear(),
+        currentWeek: currentWeek,
+    }
+}
+
 module.exports = {
-    getDateMMDDYYYY
+    getDateMMDDYYYY,
+    getWeekandYear
 }
