0dfefffdc6c371ebe412e99cbbb438c13cc906ba
Nate Stemo <natestemo@hotmail.com>
2023-08-12 16:31:41 -0500

    takes in functions from scraping utils

--- meet_metadata_scraper/getAllMeetMetaData.js
+++ meet_metadata_scraper/getAllMeetMetaData.js
@@ -2,16 +2,14 @@ const puppeteer = require('puppeteer')
 const { createCSVfromArray, writeCSV } = require('../utils/csv_utils');
 const { write } = require('fs');
 const {handleTotalAthleteString,getAmountMeetsOnPage} = require('../utils/string_utils')
-
+const {getMeetsOnPage,
+    getTableWriteCsv} = require('../utils/scraping_utils')
 
 //this could just grab the meet url?
-async function getAllMeetMetaData(filePath){
+async function getAllMeetMetaData(filePath, searchDate){
     console.log('running metadata scraper')
-    
-    
     let url = 'https://usaweightlifting.sport80.com/public/rankings/results/'
     
-   
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.setViewport({width:1500, height:1000})
@@ -36,7 +34,6 @@ async function getAllMeetMetaData(filePath){
         await page.click("div.v-date-picker-table table tbody tr td:nth-of-type(1) button.v-btn div.v-btn__content")  
     }
 
-    //apply should always be there. after clicking the date so we might not need to 
     async function clickApply(){
         console.log('clicking apply')
         await page.waitForSelector("div.v-card__actions.justify-end button.primary.my-2.v-btn.v-btn--is-elevated")
@@ -50,7 +47,7 @@ async function getAllMeetMetaData(filePath){
         await page.click('.data-table div.container.pb-0 div.s80-filter div.row.no-gutters .v-badge button.v-btn');      
     }
     
-    await clickFilter()
+    await clickFilter(page)
 
     //waits for the date picker to be available and then clicks it
     await page.waitForSelector('#date_range_start', {visible:true})
@@ -61,7 +58,7 @@ async function getAllMeetMetaData(filePath){
     
     let month = '';
     //it finds the month name being January 2011
-    while(month != 'January 2011'){
+    while(month != searchDate){
         // console.log('in loop')
         await moveBackMonth();
         month = await page.evaluate(()=>{
@@ -72,22 +69,13 @@ async function getAllMeetMetaData(filePath){
     console.log('got to', month)
     
 
-    await clickDate()
-    await clickApply() 
+    await clickDate(page)
+    await clickApply(page) 
     
     //waits for the data to actually load before we get all of the meet data
     await page.waitForNetworkIdle()
 
-    const tableHeaderData = await page.evaluate(()=>{
-        let elArr = Array.from(document.querySelectorAll(".data-table div div.v-data-table div.v-data-table__wrapper table thead tr th > span"))
-        elArr = elArr.map((x)=>{
-            return  x.textContent
-        })
-        return [...elArr.slice(0,-1),'Meet Url']
-    })
-    let headerCSV = tableHeaderData.join('|');
-    headerCSV += '\n'
-    writeCSV(filePath, headerCSV);
+    await getTableWriteCsv(filePath, page)
 
     console.log('starting scraping')
 
@@ -110,54 +98,16 @@ async function getAllMeetMetaData(filePath){
     console.log(await getPageData())
     console.log('done scraping')
 
-
     await browser.close();
 }
-//not working with the index situation
-async function getMeetUrl(index, page){
-    //click on a random element first
-    await page.click('h2.flex-shrink-0.align-self-end.subtitle-1', {waitUntil:'visible'})
-    
-    //click the elipses button on the specific element
-    await page.click(`tbody tr:nth-of-type(${index}) td.text-end button.v-btn.v-btn--icon`)        
-    
-    //wait for the view button to pop up
-    let viewBtnSelector = 'div.v-menu__content.menuable__content__active div.v-list.v-sheet div a div.v-list-item__content div.v-list-item__title';
-    await page.waitForSelector(viewBtnSelector, {waitUntil:'visible'})
-    
-    //get the href value of the view button
-    let viewBtn = 'div.v-menu__content.menuable__content__active div.v-list.v-sheet div a';
-    const meetHref = await page.$eval(viewBtn, anchor => anchor.getAttribute('href'));
-    const meetHrefNum = meetHref.split('/')[4]
-    return meetHrefNum;
-}
-
-async function getMeetsOnPage(athletesOnPage, page , filePath){
-    let allAthleteData =[];
-    for(let i = 1; i <= athletesOnPage+1; i++){
-        //can remove to have the scraper move quicker
-        meetUrl = await getMeetUrl(i, page);
-        let athleteData = await page.evaluate((index)=>{
-            let selector = ".data-table div div.v-data-table div.v-data-table__wrapper table tbody tr:nth-of-type("+ index +") td > div"
-            let elArr = Array.from(document.querySelectorAll(`${selector}`))
-            elArr = elArr.map((x)=>{
-                return  x.textContent.trim()
-            })
-            return elArr
-        },i)
-        //needs to change too
-        athleteData[athleteData.length-1] = meetUrl
-        //removes last element the non used action empty guy?
-        //athleteData.pop()
-        // console.log(athleteData)
-        allAthleteData.push(athleteData)
-    }
-
-    let weightliftingCSV = createCSVfromArray(allAthleteData);
-    writeCSV(filePath, weightliftingCSV)    
-}
 
 
+// async function start(){
+//     await getAllMeetMetaData('foo.csv', 'January 2023')
+//     console.log('deez')
+
+// }
+// start()
 
 module.exports = {
     getAllMeetMetaData: getAllMeetMetaData
