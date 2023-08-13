1f3da2a8cb23f2732914898810a7722104fa25e0
Nate Stemo <natestemo@hotmail.com>
2023-08-13 14:57:17 -0500

    imports main function and will run

--- chron.js
+++ chron.js
@@ -1,11 +1,18 @@
-let CronJob = require('cron').CronJob;
-let job = new CronJob(
-    '* * * * * *',
-    function() {
-        console.log('You will see this message every second');
-    },
-    null,
-    true,
-    'America/Los_Angeles'
-);
-// job.start() - See note below when to use this
+const CronJob = require('cron').CronJob;
+const {run: getNewMeets} = require('./main')
+// Create a cron job that runs every Thursday at 10:00 AM
+const cronExpression = '0 9 * * 4'; // Minutes(0), Hours(10), Day of Month(*), Month(*), Day of Week(4, Thursday)
+
+//this would run every second
+//'* * * * * *',
+
+const job = new CronJob(cronExpression, function() {
+    // Your code to run the program here
+    console.log('Running the program every Thursday!');
+    
+    //getNewMeets()
+    
+    //import my code to run every thursday
+}, null, true, 'UTC'); // The last parameter sets the timezone
+
+job.start(); // Start the cron job
