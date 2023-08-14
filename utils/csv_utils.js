502a09cd3eac9da4fa08196728fbd0a75f0a869a
Nate Stemo <natestemo@hotmail.com>
2023-08-14 11:30:13 -0500

    csv utilities to reset folders for retries

--- utils/csv_utils.js
+++ utils/csv_utils.js
@@ -1,6 +1,9 @@
 const fsPromise = require('fs/promises')
 const fs = require('fs');
 const csv = require('csv-parser');
+const util = require('util');
+const readdir = util.promisify(fs.readdir);
+const unlink = util.promisify(fs.unlink);
 
 async function extractMeetUrls(csvFilePath) {
     return new Promise((resolve, reject) => {
@@ -43,8 +46,61 @@ async function writeCSV(filePath, data){
     })
 }
 
+
+//function to delete the one file
+async function clearCsvFile(directory){
+    try {
+        // const file = await readdir(directory);
+        await unlink(directory);
+        
+      } catch(err) {
+        console.log(err);
+      }
+
+}
+//function to delete the folder
+async function clearCsvFolder(directory){
+    try {
+        const files = await readdir(directory);
+        const unlinkPromises = files.map(filename => unlink(`${directory}/${filename}`));
+        await Promise.all(unlinkPromises);
+        // await unlink(directory)
+      } catch(err) {
+        console.log(err);
+      }
+}
+
+async function clearCsvs(resourcePaths){
+    try {
+        for (const path of resourcePaths) {
+            try {
+                const stats = await fs.promises.stat(path);
+                if (stats.isFile()) {
+                    await fs.promises.unlink(path);
+                    console.log(`File cleared: ${path}`);
+                } else if (stats.isDirectory()) {
+                    await rimraf(path);
+                    console.log(`Folder cleared: ${path}`);
+                }
+            } catch (error) {
+                if (error.code === 'ENOENT') {
+                    console.log(`Resource not found: ${path}`);
+                } else {
+                    console.error('Error clearing resources:', error);
+                }
+            }
+        }
+    } catch (error) {
+        console.error('Error clearing resources:', error);
+    }
+}
+
 module.exports={
     createCSVfromArray,
     writeCSV,
-    extractMeetUrls
+    extractMeetUrls,
+    clearCsvs,
+    clearCsvFolder,
+    clearCsvFile,
+
 }
