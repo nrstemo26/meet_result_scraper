0ae8f856f436eb568ca2a66b242ac88bb0d1bef3
Nate Stemo <natestemo@hotmail.com>
2023-08-15 11:17:55 -0500

    selecting men's and womens weight classes

--- iwf_scraper/scrapeOneMeet.js
+++ iwf_scraper/scrapeOneMeet.js
@@ -39,7 +39,32 @@ async function scrapeOneMeet(meetUrl, filePath){
     //clicks women's sn/cj/total btn
     // await page.click('#results_womens_snatch', {
         // waitUntil: 'networkidle0'
-    // }) 
+    // })
+    
+    //get weightclass
+    let menSelector = '#men_snatchjerk div.results__title div.container div.row div.col-12 h3'
+    const mensWeightClasses = await page.evaluate((selector)=>{
+        let elArr = Array.from(document.querySelectorAll(selector))
+        elArr = elArr.map((x)=>{
+            return  x.textContent
+        })
+        return elArr
+    },menSelector)
+
+    let womenSelector = '#women_snatchjerk div.results__title div.container div.row div.col-12 h3'
+    const womenWeightClasses = await page.evaluate((selector)=>{
+        let elArr = Array.from(document.querySelectorAll(selector))
+        elArr = elArr.map((x)=>{
+            return  x.textContent
+        })
+        return elArr
+    },womenSelector)
+
+
+    // let weightClassEls = await page.$$(selector)
+    // let weightClasses = await page.evaluate(el => el.textContent, weightClassEls)
+    console.log(mensWeightClasses)
+    console.log(womenWeightClasses)
 
     await page.screenshot({path: 'foo.png', fullPage: true})
 
