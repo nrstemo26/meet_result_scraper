77c886eae1bf8ed3ea0f747a0c9064292f44c0fa
Nate Stemo <natestemo@hotmail.com>
2023-08-15 10:44:44 -0500

    pseudocode for project

---
+++ iwf_scraper/app.js
@@ -0,0 +1,17 @@
+// lets pseudo code the main project
+// build a scrape 1 meet with url
+// there are search queries in the url for
+// ?event_year=2018 
+
+// url for new weightclasses 2018 2023
+// https://iwf.sport/results/results-by-events/?event_year=2018
+
+// url for old weight classes goes from 1998 2018(no meets on 2019)
+// https://iwf.sport/results/results-by-events/results-by-events-old-bw/?event_year=2000
+
+//metadata scraper
+//needs to scrape 2 urls
+//results by events/
+//results by events/results-by-events-old-bw/
+//if we put the meet url as the whole url then we can go thru that meet metadata 
+//doc easily without
