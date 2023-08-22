055542da0d6fdf345e07ed9315ea37b6943ed084
Nate Stemo <natestemo@hotmail.com>
2023-08-22 11:34:36 -0500

    handles file path for the name

--- iwf_scraper/csv_utils.js
+++ iwf_scraper/csv_utils.js
@@ -1,26 +1,23 @@
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
-function writeCsv(data){
+function writeCsv(data, filePath){
    const header = Object.keys(data[0])
-   
 
-const csvWriter = createCsvWriter({
-  path: 'output.csv', 
-  //fieldDelimiter: "|", // doesn't work
-  header: header.map(key=> {
-    return {id: key, title: key}
+    const csvWriter = createCsvWriter({
+    path: filePath, 
+    //fieldDelimiter: "|", // doesn't work
+    header: header.map(key=> {
+        return {id: key, title: key}
+        })
+    });
+
+    csvWriter.writeRecords(data)
+    .then(() => {
+        console.log('CSV file has been written successfully');
     })
-});
-
-csvWriter.writeRecords(data)
-  .then(() => {
-    console.log('CSV file has been written successfully');
-  })
-  .catch(err => {
-    console.error('Error writing CSV file:', err);
-  });
-
-
+    .catch(err => {
+        console.error('Error writing CSV file:', err);
+    });
 }
 
 module.exports = {
