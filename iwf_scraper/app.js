f5e93817d32e46e36099227f4e367584fe5ac446
Nate Stemo <natestemo@hotmail.com>
2023-08-22 12:57:59 -0500

    initialized metadata scraper

--- iwf_scraper/app.js
+++ iwf_scraper/app.js
@@ -1,3 +1,5 @@
+const {getYearMetadata} = require('./getYearMetadata')
+
 // lets pseudo code the main project
 // build a scrape 1 meet with url
 // there are search queries in the url for
@@ -15,3 +17,40 @@
 //results by events/results-by-events-old-bw/
 //if we put the meet url as the whole url then we can go thru that meet metadata 
 //doc easily without
+let allMeetMetadata = [];
+let urls = []
+
+let yearMetadataUrl = `https://iwf.sport/results/results-by-events/?event_year=${2023}`
+getYearMetadata(yearMetadataUrl)
+
+//scrapes new weightclasses
+for (let i = 2018; i < 2024; i++){
+    let yearMetadataUrl = `https://iwf.sport/results/results-by-events/?event_year=${i}`
+    //scrape this year's metadata
+    //push to all meet metadata
+    //push urls to urls
+}
+
+let oldUrls = []
+//scrapes old weightclasses
+// for (let i = 1998; i < 2019; i++){
+    //     let yearMetadataUrl = `https://iwf.sport/results/results-by-events/results-by-events-old-bw/?event_year=${i}` 
+    //     //add an old weightclass: true
+    //     //scrape this year's metadata
+//     //push to all meet metadata
+//     //push urls to oldUrls
+// }
+
+
+
+//loop thru new meets w/ urls
+//scrape 1 meet(url,)
+//loop thru old meets w/ urls
+
+
+
+
+// get meet metadata
+// have it return an array of  ?event_id=562
+// use array to loop and scrape 1 meet with id
+//
---
+++ iwf_scraper/getYearMetadata.js
@@ -0,0 +1,11 @@
+const puppeteer = require('puppeteer')
+
+function getYearMetadata(url,filePath){
+    console.log('starting')
+
+    console.log('done')
+}
+
+module.exports = {
+    getYearMetadata: getYearMetadata
+}
