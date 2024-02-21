4c9cc3f431225648f45eef230811e4a6dc2bfb2a
Nate Stemo <natestemo@hotmail.com>
2024-02-21 14:52:03 -0600

    scraper for metadata without looking back with date filters

--- scrapers/getAllMeetMetaData.js
+++ scrapers/getAllMeetMetaData.js
@@ -18,8 +18,11 @@ async function getAllMeetMetaData(filePath, searchDate){
     })
 
     async function getMoreResultsOnPage(){
+        await page.waitForNetworkIdle()
         await page.click('div.v-select__slot div.v-input__append-inner div.v-input__icon')
+        await page.waitForNetworkIdle()
         await page.waitForSelector('div.v-menu__content')
+        await page.waitForNetworkIdle()
         await page.click('div.v-menu__content div.v-list.v-select-list.v-sheet div.v-list-item.v-list-item--link:nth-of-type(6)')
         await page.waitForNetworkIdle()
         console.log('got more results on page')
@@ -120,7 +123,72 @@ async function getAllMeetMetaData(filePath, searchDate){
     await browser.close();
 }
 
+async function getCurrentYearMetadata(filePath) {
+    console.log('running current year metadata scraper')
+    let url = 'https://usaweightlifting.sport80.com/public/rankings/results/'
+    
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setViewport({width:1500, height:1000})
+    await page.goto(url, {
+        waitUntil: 'networkidle0'
+    })
+
+    async function getMoreResultsOnPage(){
+        await page.waitForNetworkIdle()
+        await page.click('div.v-select__slot div.v-input__append-inner div.v-input__icon')
+        await page.waitForNetworkIdle()
+        await page.waitForSelector('div.v-menu__content')
+        await page.waitForNetworkIdle()
+        await page.click('div.v-menu__content div.v-list.v-select-list.v-sheet div.v-list-item.v-list-item--link:nth-of-type(6)')
+        await page.waitForNetworkIdle()
+        console.log('got more results on page')
+    }
+
+    async function getPageData(){
+        return await page.$eval(
+            ".data-table div div.v-data-table div.v-data-footer div.v-data-footer__pagination",
+            x =>  x.textContent
+        )
+    }
+       
+    //takes pagination from 30/page to 50/page
+    await getMoreResultsOnPage()
+    
+//    await page.screenshot({ path: 'date.png', fullPage: true })
+   
+   //waits for the data to actually load before we get all of the meet data
+   await page.waitForNetworkIdle()
+   await page.waitForNetworkIdle()
+   
+    // await page.screenshot({ path: 'page.png', fullPage: true })
+    await getTableWriteCsv(filePath, page)
+
+    console.log('starting scraping')
+    
+    await getMeetsOnPage(getAmountMeetsOnPage(await getPageData()), page, filePath);
+    console.log(await getPageData())
+
+    while(await handleTotalAthleteString(await getPageData())){
+        console.log('getting meet metadata...')
+        await Promise.all([
+            page.waitForNetworkIdle(),
+            page.click('.data-table div div.v-data-table div.v-data-footer div.v-data-footer__icons-after'),
+        ]);
+        console.log(await getPageData())
+        
+        await getMeetsOnPage(getAmountMeetsOnPage(await getPageData()), page, filePath)
+    }
+
+    console.log('getting resourses...')
+    console.log(await getPageData())
+    console.log('done scraping')
+
+    await browser.close();
+}
+
 
 module.exports = {
-    getAllMeetMetaData: getAllMeetMetaData
+    getAllMeetMetaData: getAllMeetMetaData,
+    getCurrentYearMetadata: getCurrentYearMetadata
 }
