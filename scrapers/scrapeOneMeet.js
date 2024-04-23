ceb9c68a5c60b8230bb27fdfe5ed04e822a8bbce
Nate Stemo <natestemo@hotmail.com>
2024-04-23 11:20:12 -0500

    removed some comments

--- scrapers/scrapeOneMeet.js
+++ scrapers/scrapeOneMeet.js
@@ -7,23 +7,24 @@ const {getAthletesOnPage} = require('../utils/scraping_utils')
 async function scrapeOneMeet(meetNumber, filePath){
     let baseUrl = 'https://usaweightlifting.sport80.com/public/rankings/results/'
     let url = baseUrl + meetNumber;
-   
-
+    
+    
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.setViewport({width:1500, height:1000})
     await page.goto(url, {
         waitUntil: 'networkidle0'
     })
-
-
-    async function getPageData(){
+    
+    
+    async function getPageData(){    
         return await page.$eval(
-            ".data-table div div.v-data-table div.v-data-footer div.v-data-footer__pagination",
-            x =>  x.textContent
+             ".data-table div div.v-data-table div.v-data-footer div.v-data-footer__pagination",
+             x =>  x.textContent
         )
     }
     
+        
     const tableHeaderData = await page.evaluate(()=>{
         let elArr = Array.from(document.querySelectorAll(".data-table div div.v-data-table div.v-data-table__wrapper table thead tr th > span"))
         elArr = elArr.map((x)=>{
@@ -33,6 +34,7 @@ async function scrapeOneMeet(meetNumber, filePath){
     })
 
     if(tableHeaderData.length > 0){
+        // console.log('we got a meet')
         let headerCSV = tableHeaderData.join('|');
         headerCSV += '\n'
         writeCSV(filePath, headerCSV);
@@ -42,28 +44,29 @@ async function scrapeOneMeet(meetNumber, filePath){
     }
 
 
+    ///hunting in here
     await getAthletesOnPage(getAmountMeetsOnPage(await getPageData()), page, filePath);
-    console.log(await getPageData())
+    // console.log(await getPageData())
 
     while(await handleTotalAthleteString(await getPageData())){
-        console.log('getting resourses...')
+        // console.log('getting resourses...')
         await Promise.all([
             page.waitForNetworkIdle(),
             page.click('.data-table div div.v-data-table div.v-data-footer div.v-data-footer__icons-after'),
         ]);
-        console.log(await getPageData())
+        // console.log(await getPageData())
         await getAthletesOnPage(getAmountMeetsOnPage(await getPageData()), page, filePath)
     }
 
-    console.log('getting resourses...')
-    console.log(await getPageData())
-    console.log('done scraping')
+    // console.log('getting resourses...')
+    // console.log(await getPageData())
+    // console.log('done scraping')
 
     await browser.close();
 }
 
 
-
+// scrapeOneMeet(444,'./meet_1.csv')
 module.exports = {
     scrapeOneMeet:scrapeOneMeet
 }
