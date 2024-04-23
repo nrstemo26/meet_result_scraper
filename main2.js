944342d2a76c9d362950501b9c4cc39decd0a41d
Nate Stemo <natestemo@hotmail.com>
2024-04-23 11:21:27 -0500

    checks if files exist on rescrapes and skips already scraped meets

--- main2.js
+++ main2.js
@@ -13,112 +13,78 @@ const { fileExists, deleteFile} = require('./utils/fs_utils')
 
 async function run (maxRetries, folderPath){
     let totalRetries = 0;
-    let meetRetries = 0;
-    let newMeetUrls = [];
-
-    let newMeets =[]
     let errorMeets = [];
-    let i = 0;
-    let meetUrl;
+    let successMeets = [];
 
     let metadataPath = `${folderPath}/metadata.csv`;
-    
+    let successPath = `${folderPath}/success.csv`
+    let errorPath = `${folderPath}/error.csv`
+
     while(totalRetries < maxRetries){
         try{
             if(await fileExists(metadataPath)){
-                //issues with the delimiter for reading csv
-                console.log('file exists')
-                //so the metadata file exists already
-                meetsArray = await readCsv(metadataPath)
+                meetsArray = await readCsv(metadataPath, '|')
             }else{
                 try{
-                    console.log('file does not exist')
-                    //no metadata file or there was an error getting it
                     await getCurrentYearMetadata(metadataPath);
-                    meetsArray = await readCsv(metadataPath)
+                    meetsArray = await readCsv(metadataPath, '|')
                 }catch(e){
                     await deleteFile(metadataPath);
                     throw new Error('')
                 }
             }
 
-            
-            // so we have a meet url and we need to 
+            //creates or reads the success file so we can skip meets 
+            //we already scraped if there was an error scraping
+            if(await fileExists(successPath)){
+                successMeets = await readCsv(successPath, ',')
+            }else{
+                await appendToCsv(successPath,Object.keys(meetsArray[0]))
+            }
+
+            //creates the error.csv if it doesn't exist already
+            if(!await fileExists(errorPath)){
+                await appendToCsv(errorPath, Object.keys(meetsArray[0]))
+            }
+
+        
             for(meet of meetsArray){
-                
+                let successUrls = successMeets.map(el=> el['Meet Url'])
                 let url = meet['Meet Url']
-                console.log(meet)
                 let path = `${folderPath}/meet_${url}.csv`
                 
-                let specificMeetRetries = 0;
-    
-                while(specificMeetRetries < 5){
-                    try{
-                        console.log('trying to scrape')
-                        await getOneMeetCsv(url, path)
-                        console.log('scraped that hoe')
-                        await appendToCsv(`${folderPath}/success.csv`,Object.values(meet))
-                
-                        break;
-                    }catch(e){
-                        console.log(specificMeetRetries)
-                        await deleteFile(path)
-                        if(specificMeetRetries > 4){
-                        
-                            await appendToCsv(`${folderPath}/error.csv`, Object.values(meet))
+                if(!successUrls.includes(url)){
+                    let specificMeetRetries = 0;
+                    while(specificMeetRetries < 5){
+                        try{
+                            await getOneMeetCsv(url, path)
+                            await appendToCsv(successPath, Object.values(meet))
                             break;
-                        }else{
-                            specificMeetRetries++;
+                        }catch(e){
+                            await deleteFile(path)
+                            if(specificMeetRetries > 4){
+                                await appendToCsv(errorPath, Object.values(meet))
+                                break;
+                            }else{
+                                specificMeetRetries++;
+                            }
                         }
                     }
                 }
             }
 
-
-            
-
-            // while(i < newMeetUrls.length){
-            //     try{
-            //         meetUrl = newMeetUrls[i]
-            //         console.log(`checking ${meetUrl}`)
-            //         if(!newMeets.includes(meetUrl)){
-            //             console.log(`${i+1} of ${newMeetUrls.length}`)
-            //             await getOneMeetCsv(meetUrl, `${folderPath}/meet_${meetUrl}.csv`)
-            //             i++
-            //             newMeets.push(meetUrl);
-            //         }else{
-            //             i++;
-            //         }
-                    
-                    
-            //     }catch(e){
-            //         if(meetRetries > 4){
-            //             errorMeets.push(newMeetUrls[i])
-            //             i++;
-            //             meetRetries = 0;
-            //         }else{
-            //             meetRetries++
-            //         }
-
-            //     }
-
             console.log('scraping successful')
-                
-            // }
             break;
         }catch(error){
             console.error(`Attempt ${totalRetries + 1} failed: ${error}`)
             totalRetries++;
-
         }
-
     }
 
     if(totalRetries === maxRetries){
         console.error(`Exceeded max retries (${maxRetries}). Scraping failed.`)
     }
     console.log('done')
-    console.log(errorMeets)
 }
 
 
