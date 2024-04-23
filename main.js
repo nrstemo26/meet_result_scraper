2cbbb17970c084783fee11f9c5d2ba3cc72a19d8
Nate Stemo <natestemo@hotmail.com>
2024-04-23 13:15:58 -0500

    moved unused files

--- main.js
+++ main.js
@@ -1,70 +1,85 @@
 //just uncomment run and call this file from command line
-
 const fs = require('fs');
 const path = require('path');
 const { makeNewMeetMetaData} = require('./scrapers/meetMetadataCsv')
 const {scrapeOneMeet: getOneMeetCsv} = require('./scrapers/scrapeOneMeet')
-const { getWeeksAndYears } = require('./utils/date_utils')
-const {extractMeetUrls, clearCsvFolder,clearCsvFile} = require('./utils/csv_utils')
-const {getAllMeetMetaData} = require('./scrapers/getAllMeetMetaData');
+const {readCsv,appendToCsv, extractMeetUrls, clearCsvFolder,clearCsvFile, sanitizeData, compareCsvs, checkData} = require('./utils/csv_utils')
+const {getAllMeetMetaData, getCurrentYearMetadata} = require('./scrapers/getAllMeetMetaData');
+const { fileExists, deleteFile, createFolder} = require('./utils/fs_utils')
 
 
-async function run (maxRetries){
-    let retries = 0; 
-    const { currentYear, currentWeek, previousYear, previousWeek} = getWeeksAndYears();
-    const currentFile = `./data/meet_metadata/${currentYear}_week_${currentWeek}.csv`;
-    const previousFile = `./data/meet_metadata/${previousYear}_week_${previousWeek}.csv`;
-    // console.log(currentFile)
-    // console.log(previousFile)
-    const csvFiles = [previousFile, currentFile];
+
+async function run (maxRetries, folderName){
+    let totalRetries = 0;
+    let successMeets = [];
+
+    // what do i need to do now
+    // ??
+    //i think this shit just works?
     
-    //new meets will go in the same folder as this
-    const outputCsvPath = `./data/weekly_updates/weekly_update_${currentYear}_week_${currentWeek}`
-    const outputFile = 'new_meet_metadata.csv'
-    const outputFileName = outputCsvPath + '/' + outputFile
-    
-    while(retries < maxRetries){
+
+    let metadataPath = `./data/${folderName}/metadata.csv`;
+    let successPath = `./data/${folderName}/success.csv`
+    let errorPath = `./data/${folderName}/error.csv`
+
+    await createFolder(`./data/${folderName}`)
+    await appendToCsv(successPath, ['Meet','Level','Date','Results','Meet Url'])
+    await appendToCsv(errorPath, ['Meet','Level','Date','Results','Meet Url'])
+
+
+    while(totalRetries < maxRetries){
         try{
-            //needs a date now January 2011 should get all meets
-            await getAllMeetMetaData(`./data/meet_metadata/${currentYear}_week_${currentWeek}.csv`, 'January 2011')
-            
-            //makes the file if it doesn't exist
-            const unmatchedOutputDir = path.join(outputCsvPath);
-            if (!fs.existsSync(unmatchedOutputDir)) {
-                fs.mkdirSync(unmatchedOutputDir, { recursive: true, });
+            if(await fileExists(metadataPath)){
+                meetsArray = await readCsv(metadataPath, '|')
+            }else{
+                try{
+                    await getCurrentYearMetadata(metadataPath);
+                    meetsArray = await readCsv(metadataPath, '|')
+                }catch(e){
+                    await deleteFile(metadataPath);
+                    throw new Error('')
+                }
             }
-            
-            
-            console.log('getting new meet metadata')
-            console.log(getWeeksAndYears())
 
-            //gets unmatched meets versus previous weeks scraping
-            await makeNewMeetMetaData(csvFiles, outputFileName, outputFile, unmatchedOutputDir)            
-           
-            // function to read CSV and get the meetUrls in an array or something
-            const newMeetUrls = await extractMeetUrls(outputFileName)
-            for(let i = 0; i < newMeetUrls.length; i++){
-                console.log(`${i+1} of ${newMeetUrls.length}`)
-                const meetUrl = newMeetUrls[i]
-                console.log('meeturl: ', meetUrl)
-                await getOneMeetCsv(meetUrl, `${outputCsvPath}/meet_${meetUrl}.csv`)
+            //reads the success file so we can skip meets already scraped if there was an error scraping
+            if(await fileExists(successPath)){
+                successMeets = await readCsv(successPath, ',')
             }
+        
+            for(meet of meetsArray){
+                let successUrls = successMeets.map(el=> el['Meet Url'])
+                let url = meet['Meet Url']
+                let path = `./data/${folderName}/meet_${url}.csv`
+                
+                if(!successUrls.includes(url)){
+                    let specificMeetRetries = 0;
+                    while(specificMeetRetries < 5){
+                        try{
+                            await getOneMeetCsv(url, path)
+                            await appendToCsv(successPath, Object.values(meet))
+                            break;
+                        }catch(e){
+                            await deleteFile(path)
+                            if(specificMeetRetries > 4){
+                                await appendToCsv(errorPath, Object.values(meet))
+                                break;
+                            }else{
+                                specificMeetRetries++;
+                            }
+                        }
+                    }
+                }
+            }
+
             console.log('scraping successful')
             break;
         }catch(error){
-            console.error(`Attempt ${retries + 1} failed: ${error}`)
-            retries++;
-
-            //clears the meet metadata file
-            await clearCsvFile(`./data/meet_metadata/${currentYear}_week_${currentWeek}.csv`)
-            //clears the meet results and the new meet metadata file
-            //doesn't delete the folder
-            await clearCsvFolder(outputCsvPath)
+            console.error(`Attempt ${totalRetries + 1} failed: ${error}`)
+            totalRetries++;
         }
-
     }
 
-    if(retries === maxRetries){
+    if(totalRetries === maxRetries){
         console.error(`Exceeded max retries (${maxRetries}). Scraping failed.`)
     }
     console.log('done')
@@ -77,4 +92,4 @@ module.exports = {
 }
 
 //param is amount of retries
-// run(5)
+run(5, 'backfill')
