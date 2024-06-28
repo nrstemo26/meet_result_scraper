eb337fac58f271a116fe545b35dc97e2e072f269
Nate Stemo <natestemo@hotmail.com>
2024-06-28 15:12:12 -0500

    set up to run with only scraping new meets in the year

--- main.js
+++ main.js
@@ -1,38 +1,55 @@
 const fs = require('fs');
 const { makeNewMeetMetaData} = require('./scrapers/meetMetadataCsv')
 const {scrapeOneMeet: getOneMeetCsv} = require('./scrapers/scrapeOneMeet')
-const {readCsv,appendToCsv} = require('./utils/csv_utils')
+const {readCsv, appendToCsv,betterWriteCSV, filterAlreadyScrapedMeets, addtoCSV} = require('./utils/csv_utils')
 const {getAllMeetMetaData, getCurrentYearMetadata} = require('./scrapers/getAllMeetMetaData');
-const { fileExists, deleteFile, createFolder} = require('./utils/fs_utils')
-
+const { fileExists,checkFile, deleteFile, createFolder} = require('./utils/fs_utils')
 
 async function run (maxRetries, folderName){
     let totalRetries = 0;
     let successMeets = [];
 
-    
     await fs.promises.mkdir(folderName,{recursive:true})
 
+    let allMetadataPath = `${folderName}/all_year_metadata.csv`;
     let metadataPath = `${folderName}/metadata.csv`;
     let successPath = `${folderName}/success.csv`
     let errorPath = `${folderName}/error.csv`
 
     await createFolder(`${folderName}`)
-    await createFolder(`${folderName}data`)
+    await createFolder(`${folderName}/data`)
     await appendToCsv(successPath, ['Meet','Level','Date','Results','Meet Url'])
     await appendToCsv(errorPath, ['Meet','Level','Date','Results','Meet Url'])
 
     while(totalRetries < maxRetries){
-        try{
-            if(await fileExists(metadataPath)){
+        const alreadyScrapedPath = 'already_scraped.csv';   
+        try{    
+            if(await checkFile(metadataPath)){
+                console.log('metadata file exists')
                 meetsArray = await readCsv(metadataPath, '|')
+                
             }else{
                 try{
-                    await getCurrentYearMetadata(metadataPath);
-                    meetsArray = await readCsv(metadataPath, '|')
+                    //get all metadata
+                    try{
+                        if((await checkFile(allMetadataPath)) === false){
+                            await getCurrentYearMetadata(allMetadataPath);
+                        }
+                    }catch(e){
+                        await deleteFile(allMetadataPath)
+                        throw new Error('failed to get all metadata')
+                    }
+                    //compare to already scraped
+                    try{
+                        meetsArray = await filterAlreadyScrapedMeets(allMetadataPath, alreadyScrapedPath);
+                        await betterWriteCSV(metadataPath, meetsArray, ['Meet','Level','Date','Results','Meet Url'], "|");
+                        await addtoCSV(alreadyScrapedPath, meetsArray, '|');
+                    }catch(e){
+                        await deleteFile(metadataPath);
+                        throw new Error('failed to filter already scraped meets')
+                    }
                 }catch(e){
-                    await deleteFile(metadataPath);
-                    throw new Error('')
+                    throw new Error(e)
                 }
             }
 
@@ -41,34 +58,37 @@ async function run (maxRetries, folderName){
                 successMeets = await readCsv(successPath, ',')
             }
         
-            for(meet of meetsArray){
-                let successUrls = successMeets.map(el=> el['Meet Url'])
-                let url = meet['Meet Url']
-                let path = `${folderName}/data/meet_${url}.csv`
+            // for(meet of meetsArray){
+            //     let successUrls = successMeets.map(el=> el['Meet Url'])
+            //     let url = meet['Meet Url']
+            //     console.log('scraping meet:', meet['Meet Url']);
+            //     let path = `${folderName}/data/meet_${url}.csv`
                 
-                if(!successUrls.includes(url)){
-                    let specificMeetRetries = 0;
-                    while(specificMeetRetries < 5){
-                        try{
-                            await getOneMeetCsv(url, path)
-                            await appendToCsv(successPath, Object.values(meet))
-                            break;
-                        }catch(e){
-                            await deleteFile(path)
-                            if(specificMeetRetries > 4){
-                                await appendToCsv(errorPath, Object.values(meet))
-                                break;
-                            }else{
-                                specificMeetRetries++;
-                            }
-                        }
-                    }
-                }
-            }
+            //     if(!successUrls.includes(url)){
+            //         console.log('this is a new meet')
+            //         let specificMeetRetries = 0;
+            //         while(specificMeetRetries < 5){
+            //             try{
+            //                 await getOneMeetCsv(url, path)
+            //                 await appendToCsv(successPath, Object.values(meet))
+            //                 break;
+            //             }catch(e){
+            //                 await deleteFile(path)
+            //                 if(specificMeetRetries > 4){
+            //                     await appendToCsv(errorPath, Object.values(meet))
+            //                     break;
+            //                 }else{
+            //                     specificMeetRetries++;
+            //                 }
+            //             }
+            //         }
+            //     }
+            // }
 
             console.log('scraping successful')
             break;
         }catch(error){
+            
             console.error(`Attempt ${totalRetries + 1} failed: ${error}`)
             totalRetries++;
         }
@@ -78,26 +98,10 @@ async function run (maxRetries, folderName){
         console.error(`Exceeded max retries (${maxRetries}). Scraping failed.`)
     }
     console.log('done')
-
-    //email new data// that turns into database writes
-    //if that's successfull delete the folder and contents
-    //set up everything to cron
-    
 }
-
-
 
 module.exports = {
     run: run
 }
 
-
-// run(5, './data/backfill')
-
-
-// so i need to get this to work weekly
-// what do i need to do weekly?
-// have an update of the upcoming national meets
-// have an update of the meet results(current iteration works)
-
-
+run(5, './data/backfill')
